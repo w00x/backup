fe51d60154514640cdb9b09c4156198409b42d93
Blas Soto <blassoto@gmail.com>
2018-05-02 11:34:46 -0300

    se agrega soporte para especificar host de la db

---
+++ redis_prod_to_dev.rb
@@ -0,0 +1,37 @@
+require "redis"
+require "redis-namespace"
+
+def backup_redis origin_env, destination_env
+  redis_conn = Redis.new(:host => "localhost", :port => 6381)
+  if origin_env == 'mafor'
+    origin_env = 'production'
+  end
+  $redis_prod = Redis::Namespace.new(origin_env, :redis => redis_conn)
+  $redis_local = Redis::Namespace.new(destination_env, :redis => redis_conn)
+
+  prod_keys = []
+  scan_index = 0
+  while true
+    result = $redis_prod.scan(scan_index)
+    prod_keys += result[1].select{|s| !s.include?('stat') && !s.include?('queues') &&
+                        !s.include?('processes') && !s.include?('ip-')}
+    scan_index = result[0].to_i
+    break if scan_index == 0
+  end
+  local_keys = []
+  scan_index = 0
+  while true
+    result = $redis_local.scan(scan_index)
+    local_keys += result[1].select{|s| !s.include?('stat') && !s.include?('queues') &&
+                        !s.include?('processes') && !s.include?('ip-')}
+    scan_index = result[0].to_i
+    break if scan_index == 0
+  end
+  local_keys.flatten.each do |key|
+    $redis_local.del(key)
+  end
+  prod_keys.flatten.each do |key|
+    val = $redis_prod.get(key)
+    $redis_local.set(key, val)
+  end
+end
